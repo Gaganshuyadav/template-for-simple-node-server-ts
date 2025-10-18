6efac3a145f854c268cad9e28b87df4e5f51f833
GaganshuYadav <vishu16aims@gmail.com>
2025-10-18 09:26:00 +0530

    server template

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-// index.js
 import express from  'express';
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
